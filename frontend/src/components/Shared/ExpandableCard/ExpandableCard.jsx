0b1cafa2f7466d85434f42a38de4b213d2a88bc4
Anni Mustonen <anni.mustonen@tuni.fi>
2026-01-12 15:46:29 +0200

    Modified for better reuseage

--- frontend/src/components/Shared/ExpandableCard/ExpandableCard.jsx
+++ frontend/src/components/Shared/ExpandableCard/ExpandableCard.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Card from "../Card/Card";
 
-const ExpandableCard = ({ title, period, company, children }) => {
+const ExpandableCard = ({ title, description, other, children }) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
     <Card className={`exp-card`} onClick={() => setExpanded(prev => !prev)}>
       <h2>{title}</h2>
-      <p>{period}<br/>{company}</p>
+      <p>{description}<br/>{other}</p>
       <div className={`details ${expanded ? 'show' : ''}`}>
         {children}
       </div>
