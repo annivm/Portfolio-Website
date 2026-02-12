3ca2f79df743aae973bcf51a44e8ae451bed8278
Anni Mustonen <anni.mustonen@tuni.fi>
2026-02-12 19:24:44 +0200

    removed introduction image

--- frontend/src/components/Introduction/Introduction.jsx
+++ frontend/src/components/Introduction/Introduction.jsx
@@ -7,15 +7,15 @@ const Introduction = forwardRef((props, ref) => {
     <section ref={ref} className="introduction">
         <section className='introduction-section'>
             <FadeInText component='h1' className='heading fade-in' delay={1000}>Anni Mustonen</FadeInText>
-            <img src="/images/Mustonen_Anni_sq.png" alt="Anni Mustonen" className="introduction-image" />
+            {/* <img src="/images/Mustonen_Anni_sq.png" alt="Anni Mustonen" className="introduction-image" /> */}
         </section>
-        <p className="introduction-text">
+        <FadeInText component='p' className="introduction-text fade-in" delay={1700}>
             ICT student graduating in 2026 with a background in biomedical laboratory science.
             I have hands-on experience from healthcare IT operations and practical coding experience gained through studies and projects, focusing on web development with React, Node.js, and SQL databases.
             <br />
             <br />
            I'm seeking a junior or entry-level software developer role.
-        </p>
+        </FadeInText>
     </section>
     )
 });
